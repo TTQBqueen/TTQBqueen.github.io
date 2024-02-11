597867aa0afb5041b6ba1b98ed3339dd80142540
TTQBqueen <ttboler@uncg.edu>
2024-02-10 20:49:26 -0500

    new stuff

---
+++ csc300x-hw3/script.js
@@ -0,0 +1,75 @@
+
+
+    function showItem1() {
+        var x = document.getElementById("hidden1");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+      function showItem2() {
+        var x = document.getElementById("hidden2");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+      function showItem3() {
+        var x = document.getElementById("hidden3");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+
+      function showItem4() {
+        var x = document.getElementById("hidden4");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+      function showItem5() {
+        var x = document.getElementById("hidden5");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+      function showItem6() {
+        var x = document.getElementById("hidden6");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+      function showItem7() {
+        var x = document.getElementById("hidden7");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+      function showItem8() {
+        var x = document.getElementById("hidden8");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+      function showItem9() {
+        var x = document.getElementById("hidden9");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
