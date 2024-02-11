0f24de251f6e01437688bce19ea01a327f5d2c45
TTQBqueen <ttboler@uncg.edu>
2024-02-11 16:29:20 -0500

    add new page

--- csc300x-hw3/script.js
+++ csc300x-hw3/script.js
@@ -73,3 +73,13 @@
           x.style.display = "none";
         }
       }
+
+      function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+      }
+      
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+      }
+
+      
